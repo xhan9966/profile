49d011152363625d3f19acb2a267e6d0496653be
Tony <xhan9966@conestogac.on.ca>
2022-07-18 23:48:58 -0400

    finish the task.

--- display_profile.js
+++ display_profile.js
@@ -1,10 +1,13 @@
 "use strict";
 
-$(document).ready( () => {
-	// display data from session storage
-	
-	$("back").click( () => {
-		
-	}); // end of click()
-	
+$(document).ready(() => {
+  // display data from session storage
+  $("#email").text(sessionStorage.getItem("email"));
+  $("#phone").text(sessionStorage.getItem("phone"));
+  $("#zip").text(sessionStorage.getItem("zip"));
+  $("#dob").text(sessionStorage.getItem("dob"));
+
+  $("#back").click(() => {
+    history.back();
+  }); // end of click()
 }); // end of ready()
